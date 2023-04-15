4bbec3315b3d1a860a14624f166fe7841ea5dd9d
Rohit-44 <tomarrohit2408@gmail.com>
2023-04-16 01:16:53 +0530

    mess page updated

--- mess-media-web/src/App.jsx
+++ mess-media-web/src/App.jsx
@@ -6,17 +6,25 @@ import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Section from './components/Section/Section'
 import Testimonial from './components/Testimonial/Testimonial'
+import Cards from './components/Cards/cards'
+import { useEffect, useState } from 'react'
+import Img from './components/Img/Img'
+
 
 
 function App() {
-  
   return (
     <div className="bg-dark">
-      <Navbar/>
+      {/* <Navbar/>
       <Header/>
       <Section/>
       <Testimonial/>
+      <Footer/> */}
+     
+      <Img/>
+      <Cards/>
       <Footer/>
+    
     </div>
   )
 }
