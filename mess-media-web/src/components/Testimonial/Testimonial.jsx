8fb46dbae16b094f47739632634af6e718470f1d
Aryan130902 <aryan.tiwari13092002@gmail.com>
2023-04-21 16:44:21 +0530

    testimonial updated

--- mess-media-web/src/components/Testimonial/Testimonial.jsx
+++ mess-media-web/src/components/Testimonial/Testimonial.jsx
@@ -1,37 +1,19 @@
 import React, { useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
+import { Testimonial_Images } from '../../constants/data';
 
 function Testimonial() {
-  const slides = [
-    {
-      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
-    },
-
-    {
-      url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
-    },
-  ];
-
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    const newIndex = isFirstSlide ? Testimonial_Images.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
 
   const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
+    const isLastSlide = currentIndex === Testimonial_Images.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
@@ -41,25 +23,45 @@ function Testimonial() {
   };
 
   return (
-    <div className='max-w-[650px] h-[580px] w-full m-auto py-16 px-4 relative group'>
-      <div
-        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-      ></div>
-      {/* Left Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+    <div className='container w-full m-auto py-16 px-4 relative group '>
+      <a
+        href='#'
+        className='p-10 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow lg:w-full md:flex-row md:w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'
+      >
+      <div className='flex-col'>
+        <img
+          className='object-cover w-96 rounded-3xl h-96 md:h-48 md:w-48 md:rounded-none md:rounded-3xl'
+          src={Testimonial_Images[currentIndex].url}
+          alt=''
+        />
+        <div className='flex justify-center font-sans font-bold text-primary py-2'>
+          Aryan Tiwari
+        </div>
+      </div>
+        <div className='flex flex-col justify-between p-4 leading-normal'>
+          <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+          </p>
+        </div>
+      </a>
+
+      <div className='hidden group-hover:block absolute top-[50%] transform -translate-y-1/2 left-0 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
       </div>
-      {/* Right Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+
+      <div className='hidden group-hover:block absolute top-[50%] transform -translate-y-1/2 right-0 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactRight onClick={nextSlide} size={30} />
       </div>
+
       <div className='flex top-4 justify-center py-2'>
-        {slides.map((slide, slideIndex) => (
+      {Testimonial_Images.map((slide, slideIndex) => (
           <div
-            key={slideIndex}
+            key={slide.id}
             onClick={() => goToSlide(slideIndex)}
-            className='text-2xl cursor-pointer'
+            className={`text-2xl cursor-pointer ${currentIndex === slideIndex ? 'text-black' : 'text-gray-400'}`}
           >
             <RxDotFilled />
           </div>
--- mess-media-web/src/constants/data.jsx
+++ mess-media-web/src/constants/data.jsx
@@ -13,3 +13,21 @@ export const Footer_link = [
     { id:1 , MessName:"Ganesh" , City:"Indore" },
     { id:2 , MessName:"Hare Krishna" , City:"Indore" }
 ]
+
+export const Testimonial_Images = [
+    {
+      id:1, url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+    },
+    {
+      id:2, url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    },
+    {
+      id:3, url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+    },
+    {
+      id:4, url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+    },
+    {
+      id:5, url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+    },
+  ];
